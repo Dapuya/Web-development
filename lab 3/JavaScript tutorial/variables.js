71ec9da3bdcb7292f7d0f5fb44c769e61a9eb69e
Dapuya <78230285+Dapuya@users.noreply.github.com>
2022-02-18 21:18:19 +0600

    upd

---
+++ lab 3/JavaScript tutorial/variables.js
@@ -0,0 +1,23 @@
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+let ourPlanet = "Earth";
+let currentUser = "Maria"
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(birthday);
+
+
+let name = "Ilya";
+
+// the expression is a number 1
+alert( `hello ${1}` ); // hello 1
+
+// the expression is a string "name"
+alert( `hello ${"name"}` ); // hello name
+
+// the expression is a variable, embed it
+alert( `hello ${name}` ); // hello Ilya
